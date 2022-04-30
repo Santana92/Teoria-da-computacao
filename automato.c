023ec0fa7c47c6172a83bad81faeec7470da440f
Lucas Santana <86127742+Santana92@users.noreply.github.com>
2022-04-30 13:01:39 -0300

    Update automato.c

--- automato.c
+++ automato.c
@@ -10,37 +10,45 @@
 typedef struct NoPilha *PtrNoPilha;
 
 //---------------------------------------------------------//
-typedef struct NoPilha{ // struct NoPilha
+typedef struct NoPilha
+{ // struct NoPilha
 
     char objeto;
     PtrNoPilha prox;
 } NoPilha;
 
 //---------------------------------------------------------//
-typedef struct{ // struct PilhaDinamica
+typedef struct
+{ // struct PilhaDinamica
 
     PtrNoPilha topo;
     int tamanho;
 } PilhaDinamica;
 
 //---------------------------------------------------------//
-void iniciaDinamica(PilhaDinamica *pilha){// inicia PilhaDinamica
+void iniciaDinamica(PilhaDinamica *pilha)
+{ // inicia PilhaDinamica
 
-    pilha->topo = NULL;                   // topo inicia em NULL
-    pilha->tamanho = 0;                   // tamanho é igual a 0
+    pilha->topo = NULL; // topo inicia em NULL
+    pilha->tamanho = 0; // tamanho é igual a 0
 } // iniciaDinamica
 
 //---------------------------------------------------------//
-bool vazioDinamica(PilhaDinamica *pilha){
-    if (pilha->topo == NULL){ // verifica se o topo é igual a NULL
+bool vazioDinamica(PilhaDinamica *pilha)
+{
+    if (pilha->topo == NULL)
+    { // verifica se o topo é igual a NULL
         printf("A pilha dinamica esta vazia");
-    }else{
+    }
+    else
+    {
         printf("Nao esta vazio");
     }
 } // vazioDinamica
 
 //---------------------------------------------------------//
-void empilhaDinamica(char letra, PilhaDinamica *pilha){ // empilha uma letra dentro da pilha
+void empilhaDinamica(char letra, PilhaDinamica *pilha)
+{ // empilha uma letra dentro da pilha
 
     PtrNoPilha aux;
     aux = (PtrNoPilha)malloc(sizeof(NoPilha));
@@ -51,10 +59,12 @@ void empilhaDinamica(char letra, PilhaDinamica *pilha){ // empilha uma letra den
 } // empilhaDinamica
 
 //---------------------------------------------------------//
-int desempilhaDinamica(PilhaDinamica *pilha){
+int desempilhaDinamica(PilhaDinamica *pilha)
+{
 
     char letra;
-    if (!(pilha->topo == NULL)){ // se for diferente de NULL vai come�ar a desempilhar
+    if (!(pilha->topo == NULL))
+    {                            // se for diferente de NULL vai come�ar a desempilhar
         PtrNoPilha aux;          // ponteiro para um no de pilha
         aux = pilha->topo;       // aux recebe topo da pilha
         letra = aux->objeto;     // letra recebe o Letra que vai desempilhar
@@ -63,27 +73,32 @@ int desempilhaDinamica(PilhaDinamica *pilha){
         pilha->tamanho--;        // diminui o tamanho
         return letra;            // retorna o Letra para a main
     }
-    else{
+    else
+    {
         printf("Pilha esta Vazia");
     }
 } // desempilhaDinamica
 
 //---------------------------------------------------------//
-void tamanhoDinamica(PilhaDinamica *pilha){
+void tamanhoDinamica(PilhaDinamica *pilha)
+{
     printf("tamanho = %d\n", pilha->tamanho);
 } // tamanhoDinamica
 
 //---------------------------------------------------------//
-void topoDinamica(PilhaDinamica *pilha){// mostra o tamanho da pilha
-    PtrNoPilha aux;                     // ponteiro nó de pilha
-    aux = pilha->topo;                  // aux recebe o proximo
-    printf("Topo = %c", aux->objeto);   // monstrando o letra do topo
+void topoDinamica(PilhaDinamica *pilha)
+{                                     // mostra o tamanho da pilha
+    PtrNoPilha aux;                   // ponteiro nó de pilha
+    aux = pilha->topo;                // aux recebe o proximo
+    printf("Topo = %c", aux->objeto); // monstrando o letra do topo
 } // topoDinamica
 
 //---------------------------------------------------------//
-void imprimeDinamica(PilhaDinamica *pilha){
+void imprimeDinamica(PilhaDinamica *pilha)
+{
     PtrNoPilha aux; // ponteiro nó de pilha
-    for (aux = pilha->topo; aux != NULL; aux = aux->prox){ // condição
+    for (aux = pilha->topo; aux != NULL; aux = aux->prox)
+    { // condição
         printf("%c", aux->objeto);
     } // for
 } // imprimeDinamica
@@ -91,37 +106,45 @@ void imprimeDinamica(PilhaDinamica *pilha){
 //---------------------------------------------------------//
 void destruirDinamica(PilhaDinamica *pilha)
 {
-    while (!(pilha->topo == NULL)){// condição
-        PtrNoPilha aux;            // ponteiro nó de pilha
-        aux = pilha->topo;         // aux recebendo o topo
-        pilha->topo = aux->prox;   // topo recebendo o proximo
-        free(aux);                 // liberando memoria
-        pilha->tamanho--;          // diminuindo o tamanho menos 1
-    }                              // while
+    while (!(pilha->topo == NULL))
+    {                            // condição
+        PtrNoPilha aux;          // ponteiro nó de pilha
+        aux = pilha->topo;       // aux recebendo o topo
+        pilha->topo = aux->prox; // topo recebendo o proximo
+        free(aux);               // liberando memoria
+        pilha->tamanho--;        // diminuindo o tamanho menos 1
+    }                            // while
 } // destruirDinamica
 
 //---------------------------------------------------------//
-void automatoPar(PilhaDinamica *pilha, char *string, int contador1, int meio){
+void automatoPar(PilhaDinamica *pilha, char *string, int contador1, int meio)
+{
     char letra;
     int contador2 = 0;
-    for (int i = 0; i < contador1; i++){
+    for (int i = 0; i < contador1; i++)
+    {
         letra = string[i];
-        if (contador2 < meio){
+        if (contador2 < meio)
+        {
             empilhaDinamica(letra, pilha);
-            printf("empilhou letra %c\n", letra);
-        }else if (contador2 >= meio){
-            if (letra == pilha->topo){
+        }
+        else if (contador2 >= meio)
+        {
+            if (letra == pilha->topo->objeto)
+            {
                 desempilhaDinamica(pilha);
-                printf("desemplilhou letra %c\n", letra);
             }
         }
         contador2++;
     }
     printf("contador2 vale %d\n", contador2);
 
-    if (pilha->topo == NULL){
+    if (pilha->topo == NULL)
+    {
         printf("palavra -> %s eh um palindromo", string);
-    }else{
+    }
+    else
+    {
         printf("palavra -> %s nao eh um palindromo", string);
     }
 
@@ -129,29 +152,37 @@ void automatoPar(PilhaDinamica *pilha, char *string, int contador1, int meio){
 }
 
 //---------------------------------------------------------//
-void automatoImPar(PilhaDinamica *pilha, char *string, int contador1, int meio){
-    
+void automatoImPar(PilhaDinamica *pilha, char *string, int contador1, int meio)
+{
+
     char letra;
     int contador2 = 0;
 
-     for (int i = 0; i < contador1; i++){
+    for (int i = 0; i < contador1; i++)
+    {
         letra = string[i];
-        if (contador2 < meio){
+
+        if (contador2 < meio)
+        {
             empilhaDinamica(letra, pilha);
-            printf("empilhou letra %c\n", letra);
-        }else if (contador2 > meio){
-            if (letra == pilha->topo){
+        }
+        else if (contador2 > meio)
+        {
+            if (letra == pilha->topo->objeto)
+            {
+
                 desempilhaDinamica(pilha);
-                printf("desemplilhou letra %c\n", letra);
             }
         }
         contador2++;
     }
-    printf("contador2 vale %d\n", contador2);
 
-    if (pilha->topo == NULL){
+    if (pilha->topo == NULL)
+    {
         printf("palavra -> %s eh um palindromo", string);
-    }else{
+    }
+    else
+    {
         printf("palavra -> %s nao eh um palindromo", string);
     }
 
@@ -159,7 +190,8 @@ void automatoImPar(PilhaDinamica *pilha, char *string, int contador1, int meio){
 }
 
 //---------------------------------------------------------//
-int main(){
+int main()
+{
 
     PilhaDinamica p;
     iniciaDinamica(&p);
@@ -172,9 +204,12 @@ int main(){
     contador1 = strlen(string);
     meio = contador1 / 2;
 
-    if (contador1 % 2 == 0){
+    if (contador1 % 2 == 0)
+    {
         automatoPar(&p, string, contador1, meio);
-    }else{
+    }
+    else
+    {
         automatoImPar(&p, string, contador1, meio);
     }
 
